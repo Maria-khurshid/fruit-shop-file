5138a9026ad93eb28ee665a595354a2f564c2443
Maria-khurshid <48955107+Maria-khurshid@users.noreply.github.com>
2019-06-15 17:01:01 +0500

    fruit shop task

---
+++ script.js
@@ -0,0 +1,13 @@
+let applePrice = document.querySelector("#apples");
+let orangePrice = document.querySelector("#oranges");
+let bananaPrice = document.querySelector("#bananas");
+function addApples() {
+    applePrice.value;
+}
+function addOranges() {
+    orangePrice.value;
+}
+
+function addBananas() {
+    orangePrice.value;
+}
